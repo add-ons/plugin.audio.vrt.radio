0d7850d77f85ca521937d4fa315c7b8e3aec19f9
Michaël Arnauts <michael.arnauts@gmail.com>
2020-06-14 02:56:44 +0200

    Use sock.sendall instead of sock.send (#20)

--- resources/lib/iptvmanager.py
+++ resources/lib/iptvmanager.py
@@ -24,7 +24,7 @@ class IPTVManager:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             sock.connect(('127.0.0.1', self.port))
             try:
-                sock.send(json.dumps(func()))  # pylint: disable=not-callable
+                sock.sendall(json.dumps(func()))  # pylint: disable=not-callable
             finally:
                 sock.close()
 
